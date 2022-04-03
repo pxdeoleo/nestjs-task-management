f131281ff81ab587a8a46cd8ba5eb347c16085cb
Pedro De Oleo <pedrodeoleoc@gmail.com>
2022-04-03 15:23:03 -0400

    add DTO, add crlf to prettier and set indent space

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './tasks.model';
 import { v4 as uuid } from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Injectable()
 export class TasksService {
@@ -10,7 +11,9 @@ export class TasksService {
     return this.tasks;
   }
 
-  createTask(title: string, description: string): Task {
+  createTask(createTaskDto: CreateTaskDto): Task {
+    const { title, description } = createTaskDto;
+
     const task: Task = {
       id: uuid(),
       title,
@@ -20,5 +23,13 @@ export class TasksService {
 
     this.tasks.push(task);
     return task;
+  }
+
+  GetTaskById(id: string): Task {
+    return this.tasks.find((task) => task.id === id);
+  }
+
+  DeleteTaskById(id: string): void {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
   }
 }
